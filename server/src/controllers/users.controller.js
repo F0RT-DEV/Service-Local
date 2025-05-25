a877eb06f918a9c390483cf4760e02464b2c8ca2
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-05-25 17:15:11 -0300

    mexendo coisa boba

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import * as userModel from '../models/users.model.js';
 
+
 export async function createUser(req, res) {
   try {
     const { name, email, password, phone, role } = req.body;
@@ -24,6 +25,6 @@ export async function createUser(req, res) {
     if (error.code === 'ER_DUP_ENTRY') {
       return res.status(409).json({ error: 'Email já cadastrado' });
     }
-    res.status(500).json({ error: 'Erro interno do servidor' });
+    res.status(500).json({ error });
   }
 }
--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -3,6 +3,13 @@ import * as UsersController from '../controllers/users.controller.js';
 
 const router = Router();
 
-router.post('/users', UsersController.createUser);
+router.post('/auth/register', UsersController.createUser);
+// router.post('/auth/register', UsersController.createUser);
+// router.get('/me', UsersController.createUser);
+// router.patch('/users/:id/status', UsersController.createUser);
+// router.get('/users/:id', UsersController.createUser);
+// router.get('/users?role=prestador', UsersController.createUser);
+
+
 
 export default router;
