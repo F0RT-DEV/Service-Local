efa24585ffd3da40aa2059f305ce69bf5427c316
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-05-25 17:28:00 -0300

    Update users.routes.js

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -7,4 +7,10 @@ router.post('/users', UsersController.createUser);
 router.get('/users', UsersController.getUsers);
 router.get('/users/:id', UsersController.getUserById);
 
+router.post('/auth/register', UsersController.createUser);
+// router.post('/auth/register', UsersController.createUser);
+// router.get('/me', UsersController.createUser);
+// router.patch('/users/:id/status', UsersController.createUser);
+// router.get('/users/:id', UsersController.createUser);
+// router.get('/users?role=prestador', UsersController.createUser);
 export default router;
