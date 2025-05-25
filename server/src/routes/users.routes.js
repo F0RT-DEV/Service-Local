9ce18aa5c421791181094d342634f3eca3041352
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-05-25 17:28:38 -0300

    Update users.routes.js

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -3,14 +3,15 @@ import * as UsersController from '../controllers/users.controller.js';
 
 const router = Router();
 
+// Rotas do Angelo
 router.post('/users', UsersController.createUser);
 router.get('/users', UsersController.getUsers);
 router.get('/users/:id', UsersController.getUserById);
 
+// Rotas do server
 router.post('/auth/register', UsersController.createUser);
-// router.post('/auth/register', UsersController.createUser);
-// router.get('/me', UsersController.createUser);
+// router.get('/me', UsersController.createUser); // descomente e ajuste conforme o controller real
 // router.patch('/users/:id/status', UsersController.createUser);
-// router.get('/users/:id', UsersController.createUser);
 // router.get('/users?role=prestador', UsersController.createUser);
+
 export default router;
