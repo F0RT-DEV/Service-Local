0decd0d8aa0a56efa07ad5b6fd9f1604f1e1f166
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-05-27 10:35:54 -0300

    Revert "Merge branch 'server'"
    This reverts commit 015b142b89781c1a9e15dbdcb8a48e25f1420e19, reversing
    changes made to 59cdd8ff6451eb5636ef94669458f8dfa5b5f7c8.

--- server/src/db.js
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-import db from "./config/knexConfig.js";
-
-dotenv.config();
-
-export default db;
--- server/src/routes/provider.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as providerController from "../controllers/provider.controller.js";
-const router = Router();
-
-// Rotas de Prestador
-router.get("/prestador", providerController.getPrestador);
-router.get("/prestador/:id", providerController.getPrestadorById);
-
-export default router;
