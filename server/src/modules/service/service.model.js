cf5758ff62af1d35de8cec87f10a1c381399304b
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-06-15 16:46:01 -0300

    .

--- server/src/modules/service/service.model.js
+++ server/src/modules/service/service.model.js
@@ -2,26 +2,31 @@ import db from "../../db.js";
 import {v4 as uuidv4} from "uuid";
 
 export async function createService(data) {
-  const id = uuidv4();
+	const id = uuidv4();
 
-  await db("services").insert({
-    id,
-    ...data,
-  });
+	await db("services").insert({
+		id,
+		...data,
+	});
 
-  const newService = await db("services").where({ id }).first();
-  return newService;
+	const newService = await db("services").where({id}).first();
+	return newService;
 }
 export async function getServiceById(id) {
 	return db("services").where({id}).first();
 }
 export async function getAllService(id) {
-	return db("services").where({});
+	returndb("services")
+		.join("categories", "services.category_id", "=", "categories.id")
+		.select(
+			"services.*",
+			"categories.name as category_name" // <- campo adicional
+		);
 }
 
 export async function getAllServicesByProviderId(providerId) {
-    return db("services").where({ provider_id: providerId });
+	return db("services").where({provider_id: providerId});
 }
 export async function getAllServicesByCategory(id) {
-  return db("services").where({ category_id: id })
+	return db("services").where({category_id: id});
 }
