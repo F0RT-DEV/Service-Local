b69669cadbb9fe8aa844a4703f892e384dc37f5d
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-06-16 10:38:29 -0300

    ..

--- server/src/routes/client/orders.routes.js
+++ server/src/routes/client/orders.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import * as orderController from "../../modules/order/order.controller.js";
+import { authenticateToken } from "../../middlewares/authMiddleware.js";
+import { verifyUserRole } from "../../middlewares/role.middleware.js";
 
 const router = express.Router();
 
-router.post("/orders", orderController.createOrder);
+router.post("/orders", authenticateToken, verifyUserRole, orderController.createOrder);
 router.get("/orders", orderController.getAllOrders);
 router.get("/orders/:id", orderController.getOrderById);
 router.put("/orders/:id", orderController.updateOrder);
